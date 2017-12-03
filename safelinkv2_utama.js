8165b8b8dbe42960f6c41646d85a6e18393cf03c
unas1990 <infoilmukesehatan@gmail.com>
2017-12-04 01:43:04 +0700

    Update safelinkv2_utama.js

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="top-10-beautiful-app-for-android-rooted.html"}
 if(x=="2"){xxx="the-best-top-10-worlds-insurance.html"}
 if(x=="3"){xxx="top-seven-7-best-auto-insurance.html"}
-if(x=="4"){xxx="top-25-propertycasualty-insurers-in-us.html"}a_to_ve[i].href="http://www.crot.bid/2017/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="top-25-propertycasualty-insurers-in-us.html"}a_to_ve[i].href="http://www.nembal.club/2017/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
